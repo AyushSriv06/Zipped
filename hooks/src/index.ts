f8b687c9f49fa25b7e6f27188113d2c4676d42d4
Ayush Srivastava <ayushsrisks@gmail.com>
2025-03-11 19:08:21 +0530

    Schema Updated

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -5,4 +5,8 @@ const app = express();
 app.post("/hooks/catch/:userId/:zipId", (req, res) => {
     const userId = req.params.userId;
     const zipId = req.params.zipId;
+
+    // store in a db a new trigger
+
+    // push it to a queue
 }) 
