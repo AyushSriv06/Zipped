0873b1ea08addffed48a0e3372a180496caf1322
Ayush Srivastava <ayushsrisks@gmail.com>
2025-03-29 16:09:28 +0530

    initail_worker_logic

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,6 +1,9 @@
+import { PrismaClient } from "@prisma/client"
 import {Kafka} from "kafkajs"
 
+
 const TOPIC_NAME = "zip-events2"
+const prismaclient = new PrismaClient();
 
 const kafka = new Kafka({
     clientId: "zip-events2",
@@ -14,8 +17,10 @@ async function main() {
     const consumer = kafka.consumer({
         groupId: "zip-events2-group",
     })
-
     await consumer.connect();
+
+    const producer =  kafka.producer();
+    await producer.connect();
 
     await consumer.subscribe({
         topic: TOPIC_NAME, 
@@ -31,7 +36,64 @@ async function main() {
                 value: message.value?.toString(),
             })
 
+            if(message.value?.toString()) {
+                return;
+            }
+
+            const parsedValue = JSON.parse(message.value?.toString() ?? "");
+            const zipRunId = parsedValue.zipRunId;
+            const stage = parsedValue.stage;
+
+            const zipRunDetails = await prismaclient.zipRun.findFirst({
+                where: {
+                    id: zipRunId
+                }, 
+                include: {
+                    zip: {
+                        include: {
+                            actions:{
+                                include: {
+                                    type: true
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            const curretAction = zipRunDetails?.zip.actions.find(x => x.sortingOrder === stage);
+
+            if(!curretAction) {
+                console.log("Current action can't be found");
+                return
+            }
+
+            if(curretAction.type.id === "email") {
+                console.log("Sending out an email")
+            }
+
+            if(curretAction.type.id === "send-sol") {
+                console.log("Sending out solana")
+            }
+ 
             await new Promise(r => setTimeout(r, 5000));
+
+            const zipId = message.value?.toString();
+            const lastStage = (zipRunDetails?.zip.actions?.length || 1) - 1;
+            
+            if(lastStage !== stage) {
+
+                await producer.send({
+                    topic: TOPIC_NAME,
+                    messages: [{
+                        value: JSON.stringify({
+                            stage: stage + 1,
+                            zipRunId
+                        })
+                    }]
+
+                }) 
+            }
 
             console.log("processing done")
             
