7dccbf4754589a06ce8540ff0088f93de7d2b547
Ayush Srivastava <ayushsrisks@gmail.com>
2025-03-29 12:47:54 +0530

    added_selectors

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -7,13 +7,13 @@ import { BACKEND_URL, HOOKS_URL } from "../config";
 import { LinkButton } from "@/components/buttons/LinkButton";
 import { useRouter } from "next/navigation";
 
-interface Zip {
+interface Zap {
     "id": string,
     "triggerId": string,
     "userId": number,
     "actions": {
         "id": string,
-        "zipId": string,
+        "zapId": string,
         "actionId": string,
         "sortingOrder": number,
         "type": {
@@ -24,7 +24,7 @@ interface Zip {
     }[],
     "trigger": {
         "id": string,
-        "zipId": string,
+        "zapId": string,
         "triggerId": string,
         "type": {
             "id": string,
@@ -34,50 +34,50 @@ interface Zip {
     }
 }
 
-function useZips() {
+function useZaps() {
     const [loading, setLoading] = useState(true);
-    const [zips, setZips] = useState<Zip[]>([]);
+    const [zaps, setZaps] = useState<Zap[]>([]);
 
     useEffect(() => {
-        axios.get(`${BACKEND_URL}/api/v1/zip`, {
+        axios.get(`${BACKEND_URL}/api/v1/zap`, {
             headers: {
                 "Authorization": localStorage.getItem("token")
             }
         })
             .then(res => {
-                setZips(res.data.zips);
+                setZaps(res.data.zaps);
                 setLoading(false)
             })
     }, []);
 
     return {
-        loading, zips
+        loading, zaps
     }
 }
 
 export default function() {
-    const { loading, zips } = useZips();
+    const { loading, zaps } = useZaps();
     const router = useRouter();
     
     return <div>
         <Appbar />
         <div className="flex justify-center pt-8">
-            <div className="max-w-screen-lg	w-full">
+            <div className="max-w-screen-lg	 w-full">
                 <div className="flex justify-between pr-8 ">
                     <div className="text-2xl font-bold">
-                        My Zips
+                        My Zaps
                     </div>
                     <DarkButton onClick={() => {
-                        router.push("/zip/create");
+                        router.push("/zap/create");
                     }}>Create</DarkButton>
                 </div>
             </div>
         </div>
-        {loading ? "Loading..." : <div className="flex justify-center"> <ZipTable zips={zips} /> </div>}
+        {loading ? "Loading..." : <div className="flex justify-center"> <ZapTable zaps={zaps} /> </div>}
     </div>
 }
 
-function ZipTable({ zips }: {zips: Zip[]}) {
+function ZapTable({ zaps }: {zaps: Zap[]}) {
     const router = useRouter();
 
     return <div className="p-8 max-w-screen-lg w-full">
@@ -88,13 +88,13 @@ function ZipTable({ zips }: {zips: Zip[]}) {
                 <div className="flex-1">Webhook URL</div>
                 <div className="flex-1">Go</div>
         </div>
-        {zips.map(z => <div className="flex border-b border-t py-4">
+        {zaps.map(z => <div className="flex border-b border-t py-4">
             <div className="flex-1 flex"><img src={z.trigger.type.image} className="w-[30px] h-[30px]" /> {z.actions.map(x => <img src={x.type.image} className="w-[30px] h-[30px]" />)}</div>
             <div className="flex-1">{z.id}</div>
-            <div className="flex-1">MAR 16, 2025</div>
+            <div className="flex-1">Nov 13, 2023</div>
             <div className="flex-1">{`${HOOKS_URL}/hooks/catch/1/${z.id}`}</div>
             <div className="flex-1"><LinkButton onClick={() => {
-                    router.push("/zip/" + z.id)
+                    router.push("/zap/" + z.id)
                 }}>Go</LinkButton></div>
         </div>)}
     </div>
